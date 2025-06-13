6288190f21ed6adc7fa9b00d2795c75bea56ec6d
AdamD <137497969+AdamD14@users.noreply.github.com>
2025-06-13 03:37:57 +0200

    Add wallet set and wallet management

---
+++ frontend/src/app/api/wallet-sets/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server';
+import { getCircleSDK } from '@/libs/circle-sdk.server';
+import { NewWalletSetFormInput } from '@chainsafe/circle-react-elements';
+
+export async function POST(request: Request) {
+  try {
+    const sdk = getCircleSDK();
+    const body = (await request.json()) as NewWalletSetFormInput;
+    const walletSet = await sdk.createWalletSet(body);
+
+    return NextResponse.json({
+      success: true,
+      data: walletSet.data?.walletSet,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error,
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const sdk = getCircleSDK();
+    const wallets = await sdk.listWalletSets();
+
+    return NextResponse.json({
+      success: true,
+      data: wallets.data?.walletSets,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error,
+      },
+      { status: 500 },
+    );
+  }
+}
