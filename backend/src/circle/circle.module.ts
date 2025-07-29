8cac1302a81aa8d0c6cc2fc023db02c2688e3bbd
AdamD <137497969+AdamD14@users.noreply.github.com>
2025-07-29 22:30:23 +0200

    feat: add circle API endpoints

--- backend/src/circle/circle.module.ts
+++ backend/src/circle/circle.module.ts
@@ -1,10 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { CircleService } from './circle.service';
+import { CircleController } from './circle.controller';
 import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
   imports: [ConfigModule],
+  controllers: [CircleController],
   providers: [CircleService],
   exports: [CircleService],
 })
