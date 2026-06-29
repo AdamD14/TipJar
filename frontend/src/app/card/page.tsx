f1e1b4584c738073765f5c1d5c68c905eab2dc37
Adam Duda <adamduda1490@gmail.com>
2026-06-29 22:20:25 +0200

    refactor: update card components to use rounded-lg styling

--- frontend/src/app/card/page.tsx
+++ frontend/src/app/card/page.tsx
@@ -2,37 +2,37 @@ export default function CardPage() {
   return (
     <div className="grid grid-cols-2 gap-6 justify-center content-center justify-items-center items-center min-h-screen py-12">
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.1463 0.0258 214.5) 0%, oklch(0.246939 0.042152 194.7689) 50%, oklch(0.1463 0.0258 214.5) 100%)',
         }}
       />
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.1608 0.0275 194.77) 0%, oklch(0.22 0.0718 188.47) 50%, oklch(0.1608 0.0275 194.77) 100%)',
         }}
       />
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.246939 0.042152 194.7689) 0%, oklch(0.22 0.0718 188.47) 50%, oklch(0.2118 0.0362 194.77) 100%)',
         }}
       />
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.2647 0.0493 205.42) 0%, oklch(0.256 0.0403 194.93) 40%, oklch(0.246939 0.042152 194.7689) 100%)',
         }}
       />
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.22 0.0718 188.47) 0%, oklch(0.246939 0.042152 194.7689) 50%, oklch(0.2647 0.0493 205.42) 100%)',
         }}
       />
       <div
-        className="w-[480px] h-[240px] rounded-xl"
+        className="w-[480px] h-[240px] rounded-lg"
         style={{
           background: 'linear-gradient(110deg in oklch, oklch(0.246939 0.042152 194.7689) 0%, oklch(0.256 0.0403 194.93) 50%, oklch(0.2647 0.0493 205.42) 100%)',
         }}
